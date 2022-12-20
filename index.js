d1172309153a073a82745bbff8c9e7af822cae11
Nihat <abdullazadenihat@gmail.com>
2022-12-20 22:46:33 +0400

    index.js changed

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 
 
-app.get('/', (req, res) => {
-   res.send('Debug')
+app.get('/auto_xp', (req, res) => {
+   res.send('Auto xp')
 })
+
+app.get('/auto_fight_hideout', (req, res) => {
+   res.send('Auto fight hideout')
+})
+
 
 
 app.listen(5000)
