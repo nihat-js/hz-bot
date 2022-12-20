6c6d719e6353e6d670caa962b77298b3c6ec617e
Nihat <abdullazadenihat@gmail.com>
2022-12-20 22:48:26 +0400

    index.js changed-2

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 
 
+app.get('/', (req, res) => {
+   res.send('This is index')
+})
+
+
 app.get('/auto_xp', (req, res) => {
    res.send('Auto xp')
 })
